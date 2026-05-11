1f8f77c638c97198941713c8d3bb5468f655999e
Dakshh B N <bndakshh25@gmail.com>
2026-05-11 13:11:13 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -3,27 +3,20 @@ package com.example;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 
-public class App {
-    public static void main(String[] args) {
-
-        ChromeOptions options = new ChromeOptions();
-
-        options.addArguments("--headless=new");        // Headless mode for Linux/CI
-        options.addArguments("--no-sandbox");          // Required in VM/container
-        options.addArguments("--disable-dev-shm-usage"); // Avoid shared memory issues
-
-        WebDriver driver = new ChromeDriver(options);
-
-        driver.get("https://www.saucedemo.com/");
-
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        WebDriver driver=new ChromeDriver();
+        driver.get("https://www.google.com/");
+       /* driver.manage().window().maximize();
         driver.findElement(By.id("user-name")).sendKeys("standard_user");
         driver.findElement(By.id("password")).sendKeys("secret_sauce");
-        driver.findElement(By.id("login-button")).click();
-
-        System.out.println("Login successful");
-
-        driver.quit();
+        driver.findElement(By.id("login-button")).click();*/
     }
 }
